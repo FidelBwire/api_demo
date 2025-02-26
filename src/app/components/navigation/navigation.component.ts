619d24f3bdead0543133004ee1ab9e397b54c5a6
Fidelis Bwire <fidelisbwire189@gmail.com>
2025-02-26 21:46:37 +0300

    HTML & CSS Overview

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Tooltip, initTWE } from 'tw-elements';
+import { NavItem } from '../../models/navigation';
 
 @Component({
   selector: 'app-navigation',
@@ -7,6 +8,29 @@ import { Tooltip, initTWE } from 'tw-elements';
   styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
+  navItems: NavItem[] = [
+    {
+      label: 'Home',
+      icon: '../../../assets/icons/home.svg',
+    },
+    {
+      label: 'About Us',
+      icon: '../../../assets/icons/navigation.svg',
+    },
+    {
+      label: 'Contact',
+      icon: '../../../assets/icons/navigation.svg',
+    },
+    {
+      label: 'Services',
+      icon: '../../../assets/icons/navigation.svg',
+    },
+    {
+      label: 'Team',
+      icon: '../../../assets/icons/navigation.svg',
+    },
+  ];
+
   ngOnInit() {
     initTWE({ Tooltip });
   }
